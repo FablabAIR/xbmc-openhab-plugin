08898cc9ae00ef2dde35cbc0d6094b8910a36bd1
Biajf <bianco.jfrancois@gmail.com>
2014-02-17 17:11:10 +0100

    Interface radio button independent

--- script.module.openhab/addon.py
+++ script.module.openhab/addon.py
@@ -34,6 +34,20 @@ class Leaf:
         self.state = state
         self.typeItem = typeItem
 
+class Radio:
+	def __init__(self, label,id):
+		self.label = label
+		self.r = RadioButton('off')
+		self.id = id
+
+	def radio_update(self):
+		if self.r.isSelected():
+			self.r.setLabel('On')
+			print(self.id)
+		else:
+			print(self.id)
+			self.r.setLabel('Off')		
+		
 class MyWindow(AddonDialogWindow):
 
 	def __init__(self, title, liste):
@@ -56,32 +70,36 @@ class MyWindow(AddonDialogWindow):
 		
 	def set_active_controls(self, liste):
 		# exemple RadioButton
-		self.radiobutton = RadioButton('Off')
-		self.placeControl(self.radiobutton, 6, 3)
-		self.connect(self.radiobutton, self.radio_update)
+		# self.radiobutton = RadioButton('Off')
+		# self.placeControl(self.radiobutton, 6, 3)
+		# self.connect(self.radiobutton, self.radio_update)
+		
+		self.radiobutton1 = Radio('Off',-1)
+		self.placeControl(self.radiobutton1.r, 4, 2)
+		self.connect(self.radiobutton1.r, self.radiobutton1.radio_update)
+		
+		self.radiobutton1 = Radio('Off',-2)
+		self.placeControl(self.radiobutton1.r, 5, 2)
+		self.connect(self.radiobutton1.r, self.radiobutton1.radio_update)
 		
 		self.i=1
 		for item in liste:
 			label_label = Label(item.label)
-			self.name = str(self.i)
-			print(self.name)
-			self.name = RadioButton('On')
+			#self.name = str(self.i)
+			#print(self.name)
+			self.tmp = Radio('On',self.i)
 			if(self.i<7):
 				self.placeControl(label_label, self.i, 0)
-				self.placeControl(self.name, self.i, 1)
+				self.placeControl(self.tmp.r, self.i, 1)
 			else:
 				self.placeControl(label_label, self.i, 2)
-				self.placeControl(self.name, self.i, 3)
-			self.connect(self.name, self.radio_update)
+				self.placeControl(self.tmp.r, self.i, 3)
+			self.connect(self.tmp.r, self.tmp.radio_update)
 			self.i=self.i+1
-	def radio_update(self):
-		# Update radiobutton caption on toggle
-		if self.radiobutton.isSelected():
-			self.radiobutton.setLabel('On')
-		else:
-			self.radiobutton.setLabel('Off')
-
-
+			
+	def generateListener(self,id):
+		return ''
+		
 
 ####Fonctions ##########
 def log(str) :
